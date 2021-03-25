1ceadc902c4e056998c883dfc5de1df24d8a4610
Nelson Ishimwe <nelson.ishimwe@gtbank.local>
2021-03-25 11:23:38 +0200

    project setup

---
+++ src/main/java/rw/co/gtbank/edwh/EdwhApplication.java
@@ -0,0 +1,13 @@
+package rw.co.gtbank.edwh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdwhApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EdwhApplication.class, args);
+    }
+
+}
---
+++ src/test/java/rw/co/gtbank/edwh/EdwhApplicationTests.java
@@ -0,0 +1,13 @@
+package rw.co.gtbank.edwh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdwhApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
